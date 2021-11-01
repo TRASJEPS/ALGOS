b39a9ad8db074904d37ae49dd3d342527da46881
TRASJEPS <71234985+TRASJEPS@users.noreply.github.com>
2021-11-01 00:56:32 -0700

    algo set3

---
+++ algostodo1.js
@@ -0,0 +1,35 @@
+// Arrays To Do 1
+
+class Node 
+{
+constructor(value, next = null)
+	{
+	this.val = value;
+	this.next = next;
+	}
+}
+
+class LinkedList
+{
+	constructor()
+	{
+	this.head = null;
+	this.size = 0;
+	}
+}
+
+// THIS IS HOW YOU ADD NODES!  Make a new head, or add to end
+add(value)
+{
+const newNode = new Node(val);
+	if(this.head === null)
+	{
+	this.head = newNode;
+	console.log(“The head node is:” + val);
+	}
+	else
+	{
+	let current = this.head;
+	
+	}
+}
